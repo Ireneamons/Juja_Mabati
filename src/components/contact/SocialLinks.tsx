b3dad14ffd69d47e3dcdeac2ae882874925f93f7
Ireneamons <ireneamons88@gmail.com>
2024-12-30 21:49:02 +0300

    Update SocialLinks.tsx

--- src/components/contact/SocialLinks.tsx
+++ src/components/contact/SocialLinks.tsx
@@ -6,13 +6,13 @@ const SocialLinks = () => {
     <div className="bg-white p-8 rounded-lg shadow-md">
       <h3 className="text-2xl font-semibold mb-6">Follow Us</h3>
       <div className="flex gap-4">
-        <a href="#" className="text-blue-600 hover:text-blue-700 transform hover:scale-110 transition-transform">
+        <a href="https://www.facebook.com/PreciseGeotech?mibextid=ZbWKwL" className="text-blue-600 hover:text-blue-700 transform hover:scale-110 transition-transform">
           <Facebook size={24} />
         </a>
         <a href="#" className="text-blue-600 hover:text-blue-700 transform hover:scale-110 transition-transform">
           <Twitter size={24} />
         </a>
-        <a href="#" className="text-blue-600 hover:text-blue-700 transform hover:scale-110 transition-transform">
+        <a href="https://www.instagram.com/jujamabatifactory?igsh=MTB4cm9pd3Z0ejdjMA==" className="text-blue-600 hover:text-blue-700 transform hover:scale-110 transition-transform">
           <Instagram size={24} />
         </a>
       </div>
